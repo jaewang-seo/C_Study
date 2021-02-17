4640e17e53b16180cbbd4be4891f1bc7dcaa2188
재왕 서 <jeawang.seo@gmail.com>
2021-02-17 10:32:13 +0900

    [2021-02-17]

--- Basic/Basic/main.c
+++ Basic/Basic/main.c
@@ -129,7 +129,6 @@ int main(void) {
 	system("pause");
 	return 0;
 }
-*/
 //연결리스트 삽입
 
 typedef struct {
@@ -183,6 +182,126 @@ int main(void) {
 	printf("OK");
 	
 	
+	system("pause");
+	return 0;
+}
+*/
+/*22강 양방향 연결 리스트
+typedef struct {
+	int data;
+	struct Node *prev;
+	struct Node *next;
+} Node;
+
+Node *head, *tail;
+
+void insert(int data) {
+	Node *node = (Node*)malloc(sizeof(Node));
+	node->data = data;
+	Node *cur;
+	cur = head->next;
+	while (cur->data < data && cur != tail)
+	{
+		cur = cur->next;
+	}
+	Node *prev = cur->prev;
+	prev->next = node;
+	node->prev = prev;
+	cur->prev = node;
+	node->next = cur;
+}
+void removeFront() {
+	Node *node = head->next; 
+	head->next = node->next;
+	Node *next = node->next;
+	next->prev = head;
+	free(node);
+} 
+
+void show() {
+	Node *cur = head->next;
+	while (cur != tail) {
+		printf("%d", cur->data);
+		cur = cur->next;
+	}
+}
+
+int main(void) {
+	head = (Node*)malloc(sizeof(Node));
+	tail = (Node*)malloc(sizeof(Node));
+	head->next = tail;
+	head->prev = head;
+	tail->next = tail;
+	tail->prev = head;
+	insert(1);
+	insert(2);
+	printf("OK");
+	system("puase");
+	return 0;
+}
+*/
+
+/*23강 스택
+
+#define SIZE 10000
+#define INF 99999999
+
+int stack[SIZE];
+int top = -1;
+
+
+void push(int data) {
+	if (top == SIZE - 1)
+	{
+		printf("오버플로우 발생");
+		return;
+	}
+	stack[++top] = data;
+}
+
+void pop() {
+	if (top == -1)
+	{
+		printf("언더플로우 발생");
+		return -INF;
+	}
+	return stack[top--];
+}
+
+void show() {
+	printf("스택의 최상단\n");
+	for (int i = top; i >= 0; i--)
+	{
+		printf("%d\n", stack[i]);
+	}
+	printf("스택의 하단\n");
+}
+
+ 
+int main(void) {
+	push(7);
+	push(5);
+	push(4);
+	pop();
+	show();
+	system("pause");
+	return 0;
+}
+*/ 
+#define INF 9999999
+
+typedef struct {
+	int data;
+	struct Node *next;
+}Node;
+
+typedef struct {
+	Node *top;
+} Stack;
+
+int main(void) {
+
+
 	system("pause");
 	return 0;
 }
