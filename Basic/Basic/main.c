38e88202d246815e0ee555108754030a795e2e5f
재왕 서 <jeawang.seo@gmail.com>
2021-02-23 15:10:55 +0900

    [2021-02-23]선택정렬 삽입정렬

--- Basic/Basic/main.c
+++ Basic/Basic/main.c
@@ -532,7 +532,6 @@ int main(void) {
 
 연결리스트를 이용한 큐
 
-*/
 #define INF 99999999
 
 typedef struct {
@@ -598,3 +597,77 @@ int main(void) {
 	system("pause");
 	return 0;
 }
+
+#include<limits.h>
+#define SIZE 1000
+
+int a[SIZE];
+
+int swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(void) {
+	int n, min, index;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		min = INT_MAX;
+		for (int j = i; j < n; j++) {
+			if (min > a[j]) {
+				min = a[j];
+				index = j;
+			}
+		}
+		swap(&a[i], &a[index]);
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d", a[i]);
+	}
+
+		system("pause");
+		return 0;
+
+}
+*/
+
+#include<limits.h>
+#define SIZE 1000
+
+int a[SIZE];
+
+int swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int main(void) {
+	int n, min, index;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	for (int i = 0; i < n - 1; i++) {
+		int j = i;
+		while (j >=0 && a[j] > a[j + 1])
+		{
+			swap(&a[i], &a[j + 1]);
+			j--;
+		}
+	}
+	
+	for (int i = 0; i < n; i++) {
+		printf("%d", a[i]);
+	}
+	system("pause");
+	return 0;
+
+
+}
+
+
