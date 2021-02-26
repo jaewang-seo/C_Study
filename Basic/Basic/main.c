1f548237e6bcaa9096550ee40506f86dcaa734b7
재왕 서 <jeawang.seo@gmail.com>
2021-02-26 19:06:28 +0900

    [2021-02-26] 이진 탐색

--- Basic/Basic/main.c
+++ Basic/Basic/main.c
@@ -896,4 +896,85 @@ int main(void) {
 		printf("%d", data);
 	}
 }
+
+
+33 순차 탐색과 이진탐색
+
+순차 탐색
+#define LENGTH 1000
+
+char **array;
+int founded;
+
+int main(void) {
+	int n;
+	char *word;
+	word = malloc(sizeof(char*) * 100);
+	scanf("%d %s", &n, word);
+	array = (char**)malloc(sizeof(char*) * n);
+	for (int i = 0; i < n; i++) {
+		array[i] = malloc(sizeof(char) *LENGTH);
+		scanf("%s", array[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		if (!strcmp(array[i], word)) {
+			printf("%d번째 원소입니다. \n", i + 1);
+			founded = 1;
+		}
+	}
+	if (!founded) printf("원소를 찾을 수 없습니다.");
+}
+
+이진탐색
+
+
 */
+#define MAX_SIZE 10000
+
+int a[MAX_SIZE];
+int founded = 0;
+
+int search(int start, int end, int target) {
+	if (start > end) return -9999;
+	int mid = (start + end) / 2;
+	if (a[mid] == target) return mid;
+	else if (a[mid] > target) return search(start, mid - 1, target);
+	else return search(mid + 1, end, target);
+}
+
+int main(void) {
+	int n, target;
+	scanf("%d &d", &n, &target);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a[i]);
+	}
+	int result = search(0, n - 1, target);
+	if (result != -9999) printf("%d번째 원소입니다.\n", result + 1);
+	else printf("원소를 찾을 수 없습니다.");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
